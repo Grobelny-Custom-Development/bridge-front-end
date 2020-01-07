04756a44a160f7032f7ea6b341b17233fcad63af
Sebastian Grobelny <sebastian@plushcare.com>
2020-01-06 20:01:40 -0800

    update to configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ new webpack.EnvironmentPlugin(['NODE_ENV', 'DEBUG']);
 
 const apiVariables = new webpack.DefinePlugin({
   // API_URL: JSON.stringify('http://localhost:8000'),
-  API_URL: JSON.stringify('https://bridge-api-dev.herokuapp.com/'),
+  API_URL: JSON.stringify('https://bridge-api-dev.herokuapp.com'),
 });
 
 module.exports = {

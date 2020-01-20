a124b1da4fdee00a06946bf1f6ee2d03fe886bab
Sebastian Grobelny <sebastian@plushcare.com>
2020-01-20 00:13:22 -0800

    updates to config

--- webpack.config.js
+++ webpack.config.js
@@ -9,34 +9,36 @@ const apiVariables = new webpack.DefinePlugin({
   API_URL: JSON.stringify('https://bridge-api-dev.herokuapp.com'),
 });
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-    ],
+const config = {
+  mode: 'production',
+  stats: {
+    colors: false,
+    hash: true,
+    timings: true,
+    assets: true,
+    chunks: true,
+    chunkModules: true,
+    modules: true,
+    children: true
   },
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: false
+      })
+    ]
+  },
+  entry: './src/client/index.js',
   plugins: [
-    apiVariables,
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new webpack.SourceMapDevToolPlugin({}),
+    apiVariables
   ],
 };
 
